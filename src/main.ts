c9a9c3b2f5ae3eabbb0c474f8dd1ca4d6f41fece
IsraelJorge <israeljorge2000@gmail.com>
2024-01-24 08:18:41 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+
+import { db, firestore } from './database/firebase'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Bem vindo ao app!')
+})
+
+app.post('/posts', async (req, res) => {
+  console.log(req.body)
+
+  const title = req.body.title
+
+  try {
+    const docRef = await firestore.addDoc(firestore.collection(db, 'posts'), {
+      title: title,
+    })
+
+    res.send(docRef.id)
+  } catch (e: unknown) {
+    console.log(e)
+
+    res.status(500).send(e)
+  }
+})
+
+app.listen(3000, () => {
+  console.log('Server is running http://localhost:3000')
+})
